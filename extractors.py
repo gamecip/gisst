25fc7ecb16095bae743d08bfb32e9264c4a849bf
Eric Kaltman <ekaltman@gmail.com>
2015-09-07 19:58:27 -0700

    MobyGames extractor, more utils Changes: - added in MobyGames extractor, still needs tests - added utils for pairwise overlap, strip non-breaking space, snake case

--- extractors.py
+++ extractors.py
@@ -12,11 +12,16 @@ import hashlib
 import shutil
 import os
 import re
-from utils import pairwise
+import requests
+from utils import (
+    pairwise,
+    pairwise_overlap,
+    merge_dicts,
+    replace_xa0,
+    snake_case
+)
 
 local_extract_store = 'extracted_data'
-
-
 
 # General Utils
 
@@ -159,24 +164,272 @@ class WikipediaExtractor(Extractor):
         table = {}
         for tr in trs:
             # Look at each pair of td tags, since infobox rows that we care about have two columns
-            for h_text, v_text in [(h.text, v.text) for h, v in pairwise(tr.find_all('td'))]:
-                # Needed since spaces is table show up as non-breaking Latin space instead of unicode space
-                # see: http://stackoverflow.com/questions/10993612/python-removing-xa0-from-string for more details
-                h_text = h_text.replace(u'\xa0', u' ')
+            for h_text, v_text in [(replace_xa0(h.text), v.text) for h, v in pairwise(tr.find_all('td'))]:
                 if h_text in WikipediaExtractor.headers_to_terms:
                     comp_term = WikipediaExtractor.headers_to_terms[h_text]
                     # removes blank lines and blank indexes
                     table[comp_term] = [x for x in re.split('\n+', v_text) if x != u'']
+
         return table
 
 
 class MobyGamesExtractor(Extractor):
+    headers = {
+        'coreGameRelease': {
+            'publisher':        u'Published by',
+            'developer':        u'Developed by',
+            'release_date':     u'Released',
+            'official_site':    u'Official Site',
+            'platform':         u'Platforms',
+            'also_for':         u'Also For'
+    },
+        'coreGameGenre': {
+            'genre':            u'Genre',
+            'perspective':      u'Perspective',
+            'theme':            u'Theme',
+            'esrb_rating':      u'ESRB Rating',
+            'sport':            u'Sport'
+        }
+    }
 
     def extract(self):
-        pass
+
+        # Figure out if specific or general url
+        # /game/{platform}/{game name} is specific, otherwise /game/{game name}
+        is_specific = re.search(r'http://www.mobygames.com/game/[a-z0-9]+/[a-z0-9\-]+', self.source.url)
+
+        if is_specific:
+            main_url = is_specific.group()
+        else:
+            main_url = re.search(r'http://www.mobygames.com/game/[a-z0-9\-]+', self.source.url)
+
+        main_page = self.get_page(main_url)
+        credits_page = self.get_page(main_url + '/credits')
+        release_page = self.get_page(main_url + '/release-info')
+        specs_page = self.get_page(main_url + '/techinfo')
+        rating_page = self.get_page(main_url + '/rating-systems')
+
+        extracted_info = merge_dicts(MobyGamesExtractor.scrape_main_page(main_page),
+                                     MobyGamesExtractor.scrape_credit_page(credits_page),
+                                     MobyGamesExtractor.scrape_release_page(release_page),
+                                     MobyGamesExtractor.scrape_specs_page(specs_page),
+                                     MobyGamesExtractor.scrape_rating_page(rating_page))
+
+        now = datetime.now(tz=pytz.utc).isoformat()
+        extracted_info['extracted_datetime'] = now
+        extracted_info['source_uri'] = self.source.url # should this be the base main_url or the specific one slurped?
+        extracted_info['source_file_hash'] = save_page_to_extract_store(self.source.url,
+                                                                        now,
+                                                                        (main_page, credits_page, release_page, specs_page, rating_page))
+        return extracted_info
 
     def validate(self):
-        pass
+        return re.search(r'www.mobygames.com/game/', self.source.url)
+
+    @staticmethod
+    def scrape_main_page(page_data):
+        main_dict = {}
+
+        if not page_data:
+            return main_dict
+
+        b = bs4.BeautifulSoup(page_data.text, 'html.parser')
+
+        # Title
+        main_dict['title'] = b.find('h1', class_='niceHeaderTitle').find('a').text
+
+        # Title Platform
+        header_platform = b.find('h1', class_='niceHeaderTitle').find('small')
+        if header_platform:
+            main_dict['platform'] = [re.sub(r'\(|\)', u'', header_platform.text)]
+
+        # Uses headers to find information in specific div, applies edit_funcs to list of terms if
+        # some processing is needed, edit_func takes a list of terms and a field name and returns a list of terms
+        # Current just adds to the main_dict from the outer scope
+        def core_box_extraction(div_id, edit_funcs):
+            for field, div_string in MobyGamesExtractor.headers[div_id].items():
+                core_div = b.find('div', id=div_id).find('div', string=div_string)
+                if core_div:
+                    main_dict[field] = reduce((lambda terms, func: func(terms, field)),
+                                              edit_funcs,
+                                              [x.text.replace(u'\xa0', u' ') for x in core_div.next_sibling()])
+
+        def clear_also_for(terms, f):
+            if f == 'also_for':
+                try:
+                    terms.remove(u'Combined View') # Not a platform!
+                except ValueError:
+                    pass
+                return terms
+
+        # Probably want official site url saved
+        def add_official_site_url(terms, f):
+            if f == 'official_site':
+                main_dict['official_site_url'] = b.find(id='coreGameRelease').find('a', string=terms[0]).href
+            return terms
+
+        # Publisher, Developer, Release Date, Platforms
+        core_box_extraction('coreGameRelease', (clear_also_for, add_official_site_url))
+        # Genre, Theme, Perspective, Sport, ESRB Rating
+        core_box_extraction('coreGameGenre', ())
+
+        return main_dict
+
+    @staticmethod
+    def scrape_credit_page(page_data):
+        main_dict = {}
+
+        if not page_data:
+            return main_dict
+
+        # Some entries have no credits for that specific version (first match)
+        # Others have recommendations for other credits if they are available (second),
+        # We don't follow that second link since this should just extract the exact root url the user requested
+        if re.search(r'There are no credits for the', page_data.text) or re.search(r'The following releases of this game have credits', page_data.text):
+            main_dict['credits'] = 'none'
+            return main_dict
+
+        b = bs4.BeautifulSoup(page_data.text, 'html.parser')
+
+        # All MobyGames credit listings in tables with credits split by contributing companies
+        # credits_dict -> {company_name: job_position: [name_of_person, ...]}
+        def extract_credits_table(table_object):
+            credits_dict = {}
+            cred_header = None
+            for tr in table_object.find_all('tr'):
+                tds = tr.find_all('td')
+                if len(tds) == 1:
+                    cred_header = tds[0].text.replace(u'\xa0', u' ')
+                    if cred_header == u'Credits':
+                        cred_header = u'General'
+                    credits_dict[cred_header] = {}
+                else:
+                    h_text = tds[0].text.replace(u'\xa0', u' ')
+                    v = [a.text.replace(u'\xa0', u' ') for a in tds[1].find_all('a')]
+                    credits_dict[cred_header][h_text] = v
+            return credits_dict
+
+        # Currently only scraping direct credits, could also scrap collaborations if that would be useful
+        main_dict['credits'] = extract_credits_table(b.find('table', summary='List of Credits'))
+
+        return main_dict
+
+
+    @staticmethod
+    def scrape_release_page(page_data):
+        main_dict = {}
+
+        if not page_data:
+            return main_dict
+
+        b = bs4.BeautifulSoup(page_data.text, 'html.parser')
+
+        # Try to get the first h2's parent, only h2's on release page are platform names
+        # If not present, there is no release information
+        try:
+            release_div = b.find('h2').parent
+        except AttributeError:
+            return main_dict
+
+        main_dict['release_platforms'] = {}
+
+        # Releases are a straight list of divs without much identifying information, thus this organization
+        # instead of a rather large for loop and condition mess
+        header_rules = (('header', 'attr'), ('header', 'rel_info'))
+        attr_rules = (('attr', 'attr'), ('attr', 'rel_info'))
+        rel_info_rules = (('rel_info', 'rel_info'),)
+        index_rule = (('rel_info', 'attr'),)
+        stop_rule = (('attr', None), ('rel_info', None))
+
+        def create_div_tuple(div):
+            if div.name == u'h2':
+                # ('header', platform name)
+                return 'header', replace_xa0(div.text)
+            elif div['class'] == u'floatholder':
+                # ('attr', {attr_name: [value, ...]})
+                return 'attr', {snake_case(replace_xa0(div.find(class_='fl').text)): [a.text for a in div.find_all('a')]}
+            elif div.find(class_='relInfo'):
+                # ('rel_info', {relInfoTitle: relInfoDetails, ...})
+                return 'rel_info', dict([(replace_xa0(r.find(class_='relInfoTitle').text),
+                                          replace_xa0(r.find(class_='relInfoDetails').text)) for r in div.find_all('relInfo')])
+            else:
+                return None, None
+
+        release_divs = map(create_div_tuple, release_div.children)
+        release_platform = None
+        rel_dict = {'releases': []}
+        for first, second in pairwise_overlap(release_divs):
+            rule = (first[0], second[0])
+            if rule in header_rules:
+                release_platform = first[1]
+                main_dict['release_platforms'][release_platform] = []
+            elif rule in attr_rules:
+                rel_dict = merge_dicts(rel_dict, first[1])
+            elif rule in rel_info_rules:
+                rel_dict['releases'].append(first[1])
+            elif rule in index_rule:
+                main_dict['release_platforms'][release_platform].append(rel_dict)
+                rel_dict = {'releases': []}
+            elif rule in stop_rule:
+                if rule[0] == 'attr':
+                    rel_dict = merge_dicts(rel_dict, first[1])
+                elif rule[0] == 'rel_info':
+                    rel_dict['releases'].append(first[1])
+                main_dict['release_platforms'][release_platform].append(rel_dict)
+                break
+
+        return main_dict
+
+    @staticmethod
+    def scrape_specs_page(page_data):
+        main_dict = {}
+        if not page_data:
+            return main_dict
+
+        b = bs4.BeautifulSoup(page_data.text, 'html.parser')
+
+        main_dict['specs'] = []
+
+        # All spec information in platform specific tables
+        for table in b.find('table', class_='techInfo'):
+            tds = table.find_all('td')[1:]  # First td is table header title
+            table_dict = dict([(snake_case(replace_xa0(h.text)),
+                               [replace_xa0(a.text) for a in v.find_all('a')]) for h, v in pairwise(tds)])
+            table_dict['platform'] = replace_xa0(table.find('thead').text)
+            main_dict['specs'].append(table_dict)
+
+        return main_dict
+
+
+    @staticmethod
+    def scrape_rating_page(page_data):
+        main_dict = {}
+        if not page_data:
+            return main_dict
+
+        b = bs4.BeautifulSoup(page_data.text, 'html.parser')
+
+        main_dict['rating_platform'] = {}
+
+        for h in b.find_all('h2'):
+            rating_dict = dict([tuple(replace_xa0(tr.text).split(u':')) for tr in h.next_sibling.find_all('tr')])
+            main_dict['rating_platform'][replace_xa0(h.text)] = rating_dict
+
+        return main_dict
+
+    @staticmethod
+    def get_page(uri):
+        try:
+            source = requests.get(uri)
+        except BaseException as e:
+            print e.message
+            return None
+        return source
+
+
+
+
+
 
 
 class GiantBombExtractor(Extractor):
--- utils.py
+++ utils.py
@@ -1,9 +1,46 @@
 __author__ = 'erickaltman'
 
-from itertools import izip
+from itertools import izip, tee
 
 # Pairwise function from: http://stackoverflow.com/questions/5389507/iterating-over-every-two-elements-in-a-list
 def pairwise(iterable):
     "s -> (s0,s1), (s2,s3), (s4, s5), ..."
     a = iter(iterable)
     return izip(a, a)
+
+# Pairwise function from: https://docs.python.org/2/library/itertools.html
+def pairwise_overlap(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+# Dictionary Merge from: http://stackoverflow.com/questions/38987/how-can-i-merge-two-python-dictionaries-in-a-single-expression
+def merge_dicts(*dict_args):
+    '''
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    '''
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
+# Replace latin non-breaking space with unicode space in unicode string
+# Needed since spaces is table show up as non-breaking Latin space instead of unicode space
+# see: http://stackoverflow.com/questions/10993612/python-removing-xa0-from-string for more details
+def replace_xa0(string):
+    if isinstance(string, unicode):
+        return string.replace(u'\xa0', u' ')
+    return string
+
+# 'Hi Everyone' -> 'hi_everyone'
+def snake_case(string):
+    if isinstance(string, unicode):
+        return string.replace(u' ', u'_').lower()
+    elif isinstance(string, str):
+        return string.replace(' ', '_').lower()
+    return string
+
+
